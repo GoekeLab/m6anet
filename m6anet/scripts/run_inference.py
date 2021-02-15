0c3dd76f3d6d8e8ae4423ed51638fd9b3d78f612
chrishendra93 <chrishendra93@gmail.com>
2021-02-15 05:48:29 +0000

    remove unused imports

--- m6anet/scripts/run_inference.py
+++ m6anet/scripts/run_inference.py
@@ -1,9 +1,5 @@
-import pandas as pd
-import numpy as np
 import os
 import torch
-import datetime
-import joblib
 import toml
 from argparse import ArgumentParser
 from argparse import ArgumentDefaultsHelpFormatter
