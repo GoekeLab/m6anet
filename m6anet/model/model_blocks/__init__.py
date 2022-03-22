a4aa232140b2d6c0dcf370631f36f1476f3908cc
chrishendra93 <chrishendra93@gmail.com>
2022-03-22 02:49:00 +0000

    add extract signal module

--- m6anet/model/model_blocks/__init__.py
+++ m6anet/model/model_blocks/__init__.py
@@ -1,10 +1,11 @@
-from .blocks import ConcatenateFeatures, DeaggregateNanopolish, Flatten, KmerMultipleEmbedding, Linear
+from .blocks import ConcatenateFeatures, DeaggregateNanopolish, Flatten, KmerMultipleEmbedding, Linear, ExtractSignal
 from .pooling_blocks import SummaryStatsAggregator, ProbabilityAttention, SummaryStatsProbability, MeanAggregator
 from .pooling_blocks import Attention, GatedAttention, KDELayer, KDEAttentionLayer, KDEGatedAttentionLayer
 from .pooling_blocks import SigmoidMaxPooling, SigmoidMeanPooling, SigmoidProdPooling
 
 
 __all__ = [
-    'ConcatenateFeatures', 'DeaggregateNanopolish', 'Flatten', 'KmerMultipleEmbedding', 'Linear', 'SummaryStatsAggregator', 'ProbabilityAttention', 'SummaryStatsProbability',
+    'ConcatenateFeatures', 'ExtractSignal', 
+    'DeaggregateNanopolish', 'Flatten', 'KmerMultipleEmbedding', 'Linear', 'SummaryStatsAggregator', 'ProbabilityAttention', 'SummaryStatsProbability',
     'Attention', 'GatedAttention', 'KDELayer', 'KDEAttentionLayer', 'KDEGatedAttentionLayer', 'SigmoidMaxPooling', 'SigmoidMeanPooling', 'SigmoidProdPooling', 'MeanAggregator'
 ]
