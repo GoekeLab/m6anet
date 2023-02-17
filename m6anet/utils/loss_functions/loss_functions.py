4275e1fbb2edc311a9e0af7715c8a227ad08cd4c
chrishendra93 <chrishendra93@gmail.com>
2023-02-17 03:59:03 +0000

    add docstrign to loss_function module

--- m6anet/utils/loss_functions/loss_functions.py
+++ m6anet/utils/loss_functions/loss_functions.py
@@ -1,17 +1,36 @@
-from multiprocessing import pool
+r"""
+This module is a collection of wrapper functions for loss functions used in m6Anet training
+"""
 import torch
-from torch import matmul, diagonal
-from torch.nn import CrossEntropyLoss
 from torch.nn import BCELoss
-from sklearn.cluster import KMeans
 
 
-def binary_cross_entropy_loss(y_pred, y_true):
+def binary_cross_entropy_loss(y_pred: torch.Tensor, y_true: torch.Tensor) -> torch.Tensor:
+    r'''
+    Wrapper for binary cross entropy loss in PyTorch
+
+            Args:
+                    y_pred (torch.Tensor): A PyTorch Tensor object containing model prediction
+                    y_true (torch.Tensor): A PyTorch Tensor object containing prediction groundtruth
+
+            Returns:
+                    loss (torch.Tensor): loss value between y_pred and y_true
+    '''
     loss = BCELoss()(y_pred.flatten(), y_true.float())
     return loss
 
 
-def weighted_binary_cross_entropy_loss(y_pred, y_true):
+def weighted_binary_cross_entropy_loss(y_pred: torch.Tensor, y_true: torch.Tensor) -> torch.Tensor:
+    r'''
+    Wrapper for weighted binary cross entropy loss in PyTorch
+
+            Args:
+                    y_pred (torch.Tensor): A PyTorch Tensor object containing model prediction
+                    y_true (torch.Tensor): A PyTorch Tensor object containing prediction groundtruth
+
+            Returns:
+                    loss (torch.Tensor): weighted loss value between y_pred and y_true
+    '''
     _, counts = torch.unique(y_true, return_counts=True)
     pos_weight, neg_weight = counts
     sample_weights = torch.where(y_true == 0, neg_weight, pos_weight)
